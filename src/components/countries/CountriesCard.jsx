24fa2b65aad7f5533c6d26ebc9aea7da89faa83c
Silvia <silviatc1993@gmail.com>
2024-03-18 12:10:47 +0100

    improved images from country card

--- src/components/countries/CountriesCard.jsx
+++ src/components/countries/CountriesCard.jsx
@@ -1,6 +1,5 @@
 import { memo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Tooltip } from 'react-tooltip';
 import '../../styles/countries/CountriesCard.scss';
 import emptyHeart from '../../images/empty-heart.png';
 import fulfilledHeart from '../../images/fulfilled-heart.png';
@@ -19,15 +18,11 @@ const CountriesCard = ({ country, isFav, favCountries }) => {
 
   return (
     <div className="country_card" onClick={handleCardClick}>
-      <img
-        className="country_card-img"
-        src={country.flags.svg}
-        alt={country.flags.alt}
-      />
+      <div className="country_card-img-container">
+        <img className="img" src={country.flags.svg} alt={country.flags.alt} />
+      </div>
+
       <p className="my-anchor-element">{country.name.official}</p>
-      <Tooltip anchorSelect=".my-anchor-element" place="top">
-        {country.name.official}
-      </Tooltip>
       <span className="country_card-heart" onClick={handleHeartClick}>
         {isFav ? (
           <img
